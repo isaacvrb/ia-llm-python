7040c0704717a20b536ce5474a9951d3a052e4df
Isaac Teixeira <isaacbh0297@gmail.com>
2025-08-26 20:07:54 -0300

    feat: adiciona integração com o groq

--- providers/groq.py
+++ providers/groq.py
@@ -4,4 +4,21 @@ import groq
 groq_client = groq.Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 def main():
-  return True
+  response = groq_client.chat.completions.create(
+    model="llama-3.3-70b-versatile",
+    messages=[
+      {
+        "role": "system",
+        "content": "Seja direto e conciso, uma única frase como resposta."
+      },
+      {
+        "role": "user",
+        "content": input("Digite uma pergunta: ")
+      }
+    ]
+  )
+
+  message = response.choices[0].message.content
+
+  print(message)
+  return message
