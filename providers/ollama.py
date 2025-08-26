9f17d47562213cdbc992fe28a14b6ce1aa413c47
Isaac Teixeira <isaacbh0297@gmail.com>
2025-08-26 19:51:22 -0300

    feat: adiciona integração com ollama

--- providers/ollama.py
+++ providers/ollama.py
@@ -2,4 +2,27 @@ import os
 import ollama
 
 def main():
-  return True
+  # response = ollama.generate(
+  #   model="llama3.1",
+  #   prompt=input("Digite uma pergunta: "),
+  #   system="Seja direto e conciso, uma única frase como resposta."
+  # )
+
+  response = ollama.chat(
+    model="llama3.1",
+    messages=[
+      {
+        "role": "system",
+        "content": "Seja direto e conciso, uma única frase como resposta."
+      },
+      {
+        "role": "user",
+        "content": input("Digite uma pergunta: ")
+      }
+    ]
+  )
+  
+  text = response["message"]["content"]
+  
+  print(text)
+  return text
